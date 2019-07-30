d498e4313bc3b4974828a63e5069a009e8b87b47
Raga Dani <raga.aidev@gmail.com>
2019-07-29 19:20:38 -0700

    defanging tested ok!

---
+++ defanging_IPaddress.js
@@ -0,0 +1,19 @@
+/**
+ * Replaces dots in the IPv4 address with the string '[.]'
+ * @param {string} address - The IPv4 address 
+ */
+function defangIPaddr(address){
+    newAddress = ''
+    for( i in address) {
+        if(address[i] == '.'){
+            newAddress = newAddress.concat('[.]');
+        }
+        else {
+            newAddress = newAddress.concat(address[i]);
+        }
+    }
+    return newAddress;
+}
+
+// Testing if the function works - Tested OK!
+console.log(defangIPaddr("1.1.1.1"));
