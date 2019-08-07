899e4aa9a78fc43508360e494e64d232e7f24604
Raga Dani <raga.aidev@gmail.com>
2019-08-07 00:13:21 -0700

    Trapping rain water WIP

--- Top Interview Questions/trapRainWater.js
+++ Top Interview Questions/trapRainWater.js
@@ -1,3 +1,12 @@
+/**
+ * Problem: 42. Trapping Rain Water (Hard)
+ * Given an elevation map find the retention of water which is the sum of all crevices of square size 1 enclosed between two respective elevations spread across the map.
+ * @param {number[]} elevation_map 
+ */
 function trapRainWater(elevation_map){
     
 }
+
+// Testing Trapping Rain Water - WIP
+let elevation = [0,1,0,2,1,0,1,3,2,1,2,1];
+console.log(trapRainWater(elevation));
